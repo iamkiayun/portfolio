5941e75e0c57a78ff107351e430bc2b7bd75c2e8
kiayun <kiayun.0218@outlook.com>
2021-11-04 09:52:55 +0800

    services

---
+++ pages/test/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <section id="hero">
+      <div class="hero container">
+        <div>
+          <h1>Hello,<span></span></h1>
+          <h1>My Name Is <span></span></h1>
+          <h1>KiaYun <span></span></h1>
+          <a href="#" type="button" class="cta">Portfolio</a>
+        </div>
+      </div>
+    </section>
+
+    <section id="services">
+      <div class="service container">
+        <div class="service-top">
+          <h1 class="section-title">Serv<span>i</span>ces</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab atque,
+            aut dignissimos nam nihil rem reprehenderit sequi tenetur velit vitae?
+            A deserunt dignissimos quis! Accusantium atque dicta doloremque neque soluta.</p>
+        </div>
+        <div class="service-bottom">
+          <div class="service-item">
+           <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png" alt="setting-icon"></div>
+            <h2>Web Design</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias architecto
+              consectetur cumque deserunt, esse excepturi laborum magni maxime nulla possimus
+              quibusdam reiciendis repudiandae rerum soluta, temporibus. Id perspiciatis placeat vero.</p>
+          </div>
+          <div class="service-item">
+           <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png" alt="setting-icon"></div>
+            <h2>Web Design</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias architecto
+              consectetur cumque deserunt, esse excepturi laborum magni maxime nulla possimus
+              quibusdam reiciendis repudiandae rerum soluta, temporibus. Id perspiciatis placeat vero.</p>
+          </div>
+          <div class="service-item">
+           <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png" alt="setting-icon"></div>
+            <h2>Web Design</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias architecto
+              consectetur cumque deserunt, esse excepturi laborum magni maxime nulla possimus
+              quibusdam reiciendis repudiandae rerum soluta, temporibus. Id perspiciatis placeat vero.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="projects">
+      <div class="projects container">
+        <div class="projects-header">
+          <h1 class="section-title">Recent<span>Projects</span></h1>
+        </div>
+        <div class="all-projects"></div>
+      </div>
+
+    </section>
+
+  </div>
+</template>
+
+<script>
+
+import "@/assets/hero.css"
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
