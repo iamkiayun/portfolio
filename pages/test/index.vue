559329412cdc337d6766bed0af84bb666f71eb1d
kiayun <kiayun.0218@outlook.com>
2021-11-04 22:47:02 +0800

    up until nav bar

--- pages/test/index.vue
+++ pages/test/index.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
+    <section id="header">
+      <div class="header container">
+        <div class="nav-bar">
+          <div class="brand">
+            <a href="#hero"><h1><span>K</span>ia <span>Y</span>un</h1></a>
+          </div>
+          <div class="nav-list">
+            <div class="hamburger"><div class="bar"></div></div>
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Services</a></li>
+              <li><a href="#">Projects</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+
     <section id="hero">
       <div class="hero container">
         <div>
@@ -41,6 +61,13 @@
               consectetur cumque deserunt, esse excepturi laborum magni maxime nulla possimus
               quibusdam reiciendis repudiandae rerum soluta, temporibus. Id perspiciatis placeat vero.</p>
           </div>
+          <div class="service-item">
+           <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png" alt="setting-icon"></div>
+            <h2>Web Design</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias architecto
+              consectetur cumque deserunt, esse excepturi laborum magni maxime nulla possimus
+              quibusdam reiciendis repudiandae rerum soluta, temporibus. Id perspiciatis placeat vero.</p>
+          </div>
         </div>
       </div>
     </section>
@@ -48,12 +75,153 @@
     <section id="projects">
       <div class="projects container">
         <div class="projects-header">
-          <h1 class="section-title">Recent<span>Projects</span></h1>
+          <h1 class="section-title">Recent <span>Projects</span></h1>
         </div>
-        <div class="all-projects"></div>
+        <div class="all-projects">
+          <div class="project-item">
+            <div class="project-info">
+              <h1>Project 1</h1>
+              <h2>Coding is Love</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquid consequatur
+                ducimus iusto molestiae officiis vel vitae! Amet, distinctio dolorum eos excepturi exercitationem
+                facere minus odio, praesentium, sequi voluptate voluptatum.</p>
+            </div>
+            <div class="project-image">
+              <img src="@/static/images/img-1.png" alt="image">
+            </div>
+          </div>
+          <div class="project-item">
+            <div class="project-info">
+              <h1>Project 2</h1>
+              <h2>Coding is Love</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquid consequatur
+                ducimus iusto molestiae officiis vel vitae! Amet, distinctio dolorum eos excepturi exercitationem
+                facere minus odio, praesentium, sequi voluptate voluptatum.</p>
+            </div>
+            <div class="project-image">
+              <img src="@/static/images/img-1.png" alt="image">
+            </div>
+          </div>
+          <div class="project-item">
+            <div class="project-info">
+              <h1>Project 3</h1>
+              <h2>Coding is Love</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquid consequatur
+                ducimus iusto molestiae officiis vel vitae! Amet, distinctio dolorum eos excepturi exercitationem
+                facere minus odio, praesentium, sequi voluptate voluptatum.</p>
+            </div>
+            <div class="project-image">
+              <img src="@/static/images/img-1.png" alt="image">
+            </div>
+          </div>
+          <div class="project-item">
+            <div class="project-info">
+              <h1>Project 4</h1>
+              <h2>Coding is Love</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquid consequatur
+                ducimus iusto molestiae officiis vel vitae! Amet, distinctio dolorum eos excepturi exercitationem
+                facere minus odio, praesentium, sequi voluptate voluptatum.</p>
+            </div>
+            <div class="project-image">
+              <img src="@/static/images/img-1.png" alt="image">
+            </div>
+          </div>
+          <div class="project-item">
+            <div class="project-info">
+              <h1>Project 5</h1>
+              <h2>Coding is Love</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquid consequatur
+                ducimus iusto molestiae officiis vel vitae! Amet, distinctio dolorum eos excepturi exercitationem
+                facere minus odio, praesentium, sequi voluptate voluptatum.</p>
+            </div>
+            <div class="project-image">
+              <img src="@/static/images/img-1.png" alt="image">
+            </div>
+          </div>
+        </div>
       </div>
 
     </section>
+
+    <section id="about">
+      <div class="about container">
+        <div class="col-left">
+          <div class="about-img">
+<!--            <img src="@/static/images/img-2.png" alt="img">-->
+          </div>
+        </div>
+        <div class="col-right">
+          <h1 class="section-title">About <span>me</span></h1>
+          <h2>Front End Developer</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam magnam necessitatibus
+            odio sapiente? Assumenda consequatur cupiditate delectus dolore dolorum labore laudantium
+            neque nisi, nobis omnis praesentium quas quidem reprehenderit ullam? Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Ab aliquid asperiores beatae blanditiis consequatur, cum dolorum
+            eligendi iure laborum, minima, nemo placeat voluptate voluptatem? Cum distinctio magnam quos
+            unde voluptate?</p>
+          <a href="#" class="cta">Download Resume</a>
+        </div>
+      </div>
+    </section>
+
+    <section id="contact">
+      <div class="contact container">
+        <div><h1 class="section-title">Contact <span>Info</span></h1></div>
+        <div class="contact-items">
+          <div class="contact-item">
+            <div class="icon"><img src="https://img.icons8.com/bubbles/50/000000/phone--v1.png"/></div>
+            <div class="contact-info">
+              <h1>Phone</h1>
+              <h2>+6017 xxxx xxx</h2>
+              <h2>+6017 xxxx xxx</h2>
+            </div>
+          </div>
+            <div class="contact-item">
+            <div class="icon"><img src="https://img.icons8.com/bubbles/50/000000/new-post.png"/></div>
+            <div class="contact-info">
+              <h1>Email</h1>
+              <h2>info@gmail.com</h2>
+              <h2>info@gmail.com</h2>
+            </div>
+          </div>
+            <div class="contact-item">
+            <div class="icon"><img src="https://img.icons8.com/bubbles/50/000000/map-marker.png"/></div>
+            <div class="contact-info">
+              <h1>Address</h1>
+              <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit.  </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+    </section>
+
+    <section id="footer">
+      <div class="footer container">
+        <div class="brand"><h1><span>K</span>ia <span>Y</span>un</h1></div>
+        <h2>Your Complete Web Solution</h2>
+        <div class="social-icon">
+          <div class="social-item">
+            <a href="#"><img src="https://img.icons8.com/bubbles/50/000000/facebook-new.png"/></a>
+          </div>
+            <div class="social-item">
+            <a href="#"><img src="https://img.icons8.com/bubbles/50/000000/instagram-new.png"/></a>
+          </div>
+            <div class="social-item">
+            <a href="#"><img src="https://img.icons8.com/bubbles/50/000000/twitter.png"/></a>
+          </div>
+            <div class="social-item">
+            <a href="#"><img src="https://img.icons8.com/bubbles/50/000000/behance.png"/></a>
+          </div>
+        </div>
+        <p>Copyright @ 2021 KiaYun. All rights reserved.</p>
+      </div>
+
+
+
+    </section>
+
 
   </div>
 </template>
